ac3b238ad1399ba4965e131cd52bf9bedb500eb8
Rao Dasika <rao.dasika@ieee.org>
2023-05-15 15:24:10 -0400

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require('fs')
+
+console.log('start')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  console.log("\n read the first file")
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    console.log("\n read the second file\n")
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log('wrote first and second files')
+      }
+    )
+  })
+})
+console.log('starting next task')
